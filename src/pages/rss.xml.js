78b064fbbcb6dc3f6a2108faf13a74e106c58324
Àlvar Pérez <alvarperezrelano@gmail.com>
2023-09-29 23:02:05 +0200

    Fix RSS feed generation

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,17 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from "@astrojs/rss";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import { getBlogPosts } from "../content/getBlogPosts";
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+  const posts = await getBlogPosts();
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map((post) => ({
+      ...post,
+      pubDate: new Date(post.date),
+      link: `/blog/${post.id}/`,
+    })),
+  });
 }
